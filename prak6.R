08f58d852843a785e932e925f8dd421b3d5bc785
JackyLeg <91046882+JackyLeg@users.noreply.github.com>
2022-10-20 22:09:40 +0700

    add latihan 4 and giving comment
    completing modul 6 statistika and giving comment for information of the code

--- prak6.R
+++ prak6.R
@@ -1,4 +1,4 @@
-#1
+#1 - Boxplot
 iris = read.csv("D:/iris.csv")
 View(iris)
 summary(iris)
@@ -9,7 +9,7 @@ var(iris$sepal_length)
 boxplot(iris$petal_length)
 boxplot(petal_length ~ species, data = iris, main = "Petal Length", xlab = "Species", ylab = "Length")
 
-#2
+#2 - Histogram 'n his density
 hist(iris$petal_length)
 hist(iris$sepal_width)
 hist(iris$sepal_width[iris$species == "setosa"])
@@ -19,22 +19,24 @@ plot(d)
 d = density(iris$sepal_width[iris$species == "versicolor"])
 plot(d)
 
-#3
+#3 - Scatter Plot
 #install.packages("ggplot2")
+#^ must install, if you haven't ^
 library(ggplot2)
 data_made = read.csv("D:/DirectMarketing.csv")
 View(data_made)
 qplot(data_made$AmountSpent, data_made$Salary)
 plot(data_made$AmountSpent, data_made$Salary)
 
-#4
+#4 - Histogram + Density of AmountSpent from DirectMarketing.csv file
 library(ggplot2)
 data_novandy = read.csv("D:/DirectMarketing.csv")
 View(data_novandy)
+hist(data_novandy$AmountSpent)
 plot_kece = density(data_novandy$AmountSpent)
 plot(plot_kece)
 
-#4b
+#4b - Compare var Price with other numeric var
 data_asardi = read.csv("D:/houseprice.csv")
 View(data_asardi)
 
